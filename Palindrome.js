4d84a586696f7fd8a418954d6b7a4d5614f4f18c
rakeshd1127 <rocky1127boii@gmail.com>
2025-09-05 15:48:28 +0530

    Create Palindrome.js

---
+++ Palindrome.js
@@ -0,0 +1,8 @@
+// palindrome.js
+function isPalindrome(str) {
+    let reversed = str.split("").reverse().join("");
+    return str === reversed;
+}
+
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("hello")); // false
