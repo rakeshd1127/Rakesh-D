4c1eb0f54ceb8a3790e664fff0ddd5feae6fc56f
rakeshd1127 <rocky1127boii@gmail.com>
2025-09-05 15:47:55 +0530

    Create Object.js

---
+++ Object.js
@@ -0,0 +1,11 @@
+// object.js
+
+// Object literal
+const person = {
+    name: "Robert",
+    age: 30,
+    city: "London"
+};
+
+console.log(person.name);  // Robert
+console.log(person["age"]); // 30
